7c0343b32fa059f753b7ff95cfe3a13da875139f
pervezmia <ahmedpervezkabir@gmail.com>
2026-03-30 11:28:54 +0600

    buy now btn conditionally worked

--- src/components/DigitalTools.jsx
+++ src/components/DigitalTools.jsx
@@ -1,9 +1,10 @@
 import React, { use, useState } from "react";
+import DigitalToolsCard from "./DigitalToolsCard";
 
 const DigitalTools = ({ productsPromise }) => {
   const products = use(productsPromise);
   console.log(products);
-  const [isBuy, setIsBuy] = useState(false)
+  
   return (
     <div className="space-y-10">
       <div>
@@ -38,30 +39,7 @@ const DigitalTools = ({ productsPromise }) => {
       <div className=" max-w-[70%] mx-auto grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 container">
         {products.map((product) => (
           <div className="card" key={product.id}>
-            <div className="p-6 rounded-xl border border-gray-400 space-y-4">
-              <div className="flex items-center justify-between">
-                <img
-                  className="w-10 h-10 object-contain"
-                  src={product.icon}
-                  alt={product.name}
-                />
-                <p>{product.tagType}</p>
-              </div>
-              <div>
-                <h3 className="font-bold text-2xl">{product.name}</h3>
-                <p className="text-gray-600"> {product.description} </p>
-              </div>
-              <div className="flex items-center">
-                <p className="font-bold text-2xl ">${product.price}</p>/
-                {product.period}
-              </div>
-              <div>
-                <p>{product.period}</p>
-              </div>
-              <button onClick={() => setIsBuy(true)} className="btn w-full rounded-full bg-[#9514FA] text-white font-semibold">
-                {isBuy? "Added to Cart" : "Buy Now" }
-              </button>
-            </div>
+            <DigitalToolsCard product = {product}></DigitalToolsCard>
           </div>
         ))}
       </div>
--- src/components/DigitalToolsCard.jsx
+++ src/components/DigitalToolsCard.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const DigitalToolsCard = () => {
+
+const DigitalToolsCard = ({product}) => {
+    const [isBuy, setIsBuy] = useState(false)
+    
     return (
-        <div>
-            
-        </div>
+        <div className="p-6 rounded-xl border border-gray-400 space-y-4">
+              <div className="flex items-center justify-between">
+                <img
+                  className="w-10 h-10 object-contain"
+                  src={product.icon}
+                  alt={product.name}
+                />
+                <p>{product.tagType}</p>
+              </div>
+              <div>
+                <h3 className="font-bold text-2xl">{product.name}</h3>
+                <p className="text-gray-600"> {product.description} </p>
+              </div>
+              <div className="flex items-center">
+                <p className="font-bold text-2xl ">${product.price}</p>/
+                {product.period}
+              </div>
+              <div>
+                <p>{product.period}</p>
+              </div>
+              <button onClick={() => setIsBuy(true)} className="btn w-full rounded-full bg-[#9514FA] text-white font-semibold">
+                {isBuy? "Added to Cart" : "Buy Now" }
+              </button>
+            </div>
     );
 };
 
